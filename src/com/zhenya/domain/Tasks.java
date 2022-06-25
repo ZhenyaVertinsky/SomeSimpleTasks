e99a0632c3967d25f1c1ae41dd2aa4d77c55c2a7
Евгений <Zhenya.vertinsky@gmail.com>
2022-06-25 22:40:05 +0300

    Initial commit1

--- src/com/zhenya/domain/Tasks.java
+++ src/com/zhenya/domain/Tasks.java
@@ -1,20 +1,19 @@
 package com.zhenya.domain;
 
 public class Tasks {
-    /* Task 1. Напишите программу, которая вычисляет ваш вес на Луне с учетом того,
-        что сила тяжести равна 16% от силы тяжести на Земле. */
+    // Task 1.
     public static double getWeight(double weightMan, double percent) {
         return weightMan * percent;
     }
 
-    // Task 2. Напишите метод, который будет увеличивать каждый элемент массива на 10%.
+    // Task 2.
     public static void arrayExtraValue(int[] array, double percent) {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i] * percent + array[i]);
         }
     }
 
-    // Task 3. Вывести на экран все четные цифры от 1 до 100 (используйте остаток от деления)
+    // Task 3.
     public static void evenNumber(int evenFrom, int evenTo) {
         for (int i = evenFrom; i <= evenTo; i++) {
             if (i % 2 == 0)
@@ -23,7 +22,7 @@ public class Tasks {
         System.out.println();
     }
 
-    // Task 4. Вывести на  экран все нечетные цифры от 1 до 15 (используйте остаток от деления)
+    // Task 4.
     public static void notEvenNumber(int notEvenFrom, int notEvenTo) {
         for (int i = notEvenFrom; i <= notEvenTo; i++) {
             if (i % 2 != 0)
@@ -32,8 +31,7 @@ public class Tasks {
         System.out.println();
     }
 
-    /* Task 5. Вывести на экран все четные цифры от 1 до 100
-    не используя остаток от деления (используя только цикл for) */
+    // Task 5.
     public static void evenNumber2(int evenWithoutPercentFrom, int evenWithoutPercentTwo) {
         for (int i = evenWithoutPercentFrom; i <= evenWithoutPercentTwo; i += 2) {
             System.out.print(i + " ");
@@ -41,7 +39,7 @@ public class Tasks {
         System.out.println();
     }
 
-    // Task 6. Посчитать сумму цифр от 20 до 200 и результат вывести на экран (используя цикл for)
+    // Task 6.
     public static int sumOfNumbers(int start, int end) {
         int sum = 0;
         for (int i = start; i <= end; i++) {
@@ -50,8 +48,7 @@ public class Tasks {
         return sum;
     }
 
-    /* Task 7. Пройти циклом по числам от 1 до 12 и вывести название месяца,
-    соответствующее каждому числу (используя цикл и оператор if else) */
+    // Task 7.
     public static void arrayMonths(int[] months) {
         for (int i = 1; i <= months.length; i++) {
             if (i == months[0])
@@ -81,8 +78,7 @@ public class Tasks {
         }
     }
 
-    /* Task 8. Объявить 2 переменных целого типа. Необходимо поменять значения переменных так,
-    чтобы значение первой оказалось во второй, а второй - в первой. */
+    // Task 8.
     public static void replaceValue(int first, int second) {
         int third = first;
         first = second;
@@ -91,14 +87,13 @@ public class Tasks {
         System.out.println(second);
     }
 
-    /* Task 9. Напишите программу с тремя переменными целого типа, первым двум присвойте значения,
-    а третьей переменной присвойте их сумму. Выведете значение третей переменной на экран. */
+    // Task 9.
     public static void sumOfNumber(int numberOne, int numberTwo) {
         int numberThree = numberOne + numberTwo;
         System.out.println(numberThree);
     }
 
-    // Task 10. Напишите программу с тремя переменными целого типа. Определить какое из них наибольшее.
+    // Task 10.
     public static void MaxValueFromAll(int firstValue, int secondValue, int thirdValue) {
         if (firstValue > secondValue) {
             if (firstValue > thirdValue) {
@@ -117,25 +112,19 @@ public class Tasks {
         }
     }
 
-    /* Task 11. Напишите программу, в которой используются две переменные логического типа (boolean),
-    присвойте им значения и выведете для каждой переменной на экран строку "истина",
-    если переменная имеет значение true и "ложь", если переменная имеет значение false. */
+    // Task 11.
     public static void logic (boolean typeOne, boolean typeTwo) {
         System.out.println("Truth " + typeOne);
         System.out.println("Lie " + typeTwo);
     }
 
-    /* Task 12. Напишите программу, где двум строковым переменным присваиваются значения,
-    третей же строковой переменной присвойте объединение (конкатенацию) двух предыдущих строк.
-    Затем напечатайте значение третьей строковой переменной. */
+    // Task 12.
     public static void stringValue (String nameOne, String nameTwo) {
         String nameThree = nameOne + nameTwo;
         System.out.println(nameThree);
     }
 
-    /* Task 13. Создайте программу с двумя переменными целого типа, присвойте им значения,
-    если первая переменная больше второй, то увеличьте её значение на 3,
-    иначе увеличьте значение второй переменной на 8. В конце программы выведите значения обоих чисел на экран. */
+    // Task 13.
     public static void volumePlus(int volumePlusNumber, int volumePlusNumberTwo) {
         if (volumePlusNumber > volumePlusNumberTwo) {
             System.out.println(volumePlusNumber + 3);
@@ -144,8 +133,7 @@ public class Tasks {
         }
     }
 
-    /* Task 14. Выведете на экран все числа от 1 до 100 с помощью цикла
-    (можно все три варианта в одной программе): for while do while. */
+    // Task 14.
     public static void loopForNumbers(int startFirstNumber, int lastNumber) {
         do {
             System.out.print(startFirstNumber++ + " ");
@@ -154,16 +142,14 @@ public class Tasks {
         System.out.println();
     }
 
-    /* Task 15. Вывести на экран ряд натуральных чисел от минимума до максимума с шагом.
-    Например, если минимум 10, максимум 35, шаг 5, то вывод должен быть таким: 10 15 20 25 30 35.
-    Минимум, максимум и шаг указываются пользователем (можно захардкодить). */
+    // Task 15.
     public static void naturalNumbersWithStep (int minValueForStep, int maxValueForStep, int stepValue) {
         for (int i = minValueForStep; i <= maxValueForStep; i+= stepValue) {
             System.out.println(i);
         }
     }
 
-    // Task 16. Все элементы массива поделить на значение наибольшего элемента этого массива.
+    // Task 16.
     public static void arraySplit(double [] arrayCount) {
         double max = arrayCount[0];
         for (int i = 0; i < arrayCount.length; i++) {
@@ -177,8 +163,7 @@ public class Tasks {
         }
     }
 
-    /* Task 17. Напишите метод переводящий рубли в доллары по заданному курсу.
-    В качестве аргументов укажите кол-во рублей и курс. */
+    // Task 17.
     public static void currencyConverter (int rublesBlr, double exchangeRateDollars) {
         double converter = rublesBlr / exchangeRateDollars;
         System.out.println(converter + " $");
